faf59e61941b1cacb9204e48115976755e92af72
Deep Makadiya <deepmac.25@gmail.com>
2021-06-03 05:29:14 +0530

    feat: fa added with responsive navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,29 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Navbar.styles.css';
 import Logo from '../../Assets/wd_logo_nav.png';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const imgRef = useRef();
-  useEffect(() => {
-    window.addEventListener('scroll', function () {
-      const navbarLogo = imgRef;
-      let value = window.scrollY;
-      navbarLogo.current.style.right = `${value * 0.06}px`;
-    });
-  });
+  const [showNav, setShowNav] = useState(false);
+  // useEffect(() => {
+  //   window.addEventListener('scroll', function () {
+  //     const navbarLogo = imgRef;
+  //     let value = window.scrollY;
+  //     navbarLogo.current.style.right = `${value * 0.06}px`;
+  //   });
+  // });
 
   return (
     <nav className="navbar">
       <div className="navbar__logoContainer">
         <img ref={imgRef} id="logoImg" className="navbar__logoImg" src={Logo} alt="logo" />
       </div>
-      <ul className="navbar__nav">
+      <div className="navbar__burgerBtn" onClick={() => setShowNav(!showNav)}>
+        <i class={showNav ? 'fas fa-times' : 'fas fa-bars'}></i>
+      </div>
+
+      <ul className={showNav ? 'navbar__nav active' : 'navbar__nav'}>
         <li>
           <a href="/">Home</a>
         </li>
