0facbadbbb78463e3405cbef33ec0f30cd87ea3e
Deep Makadiya <deepmac.25@gmail.com>
2021-06-16 12:44:47 +0530

    style: lots of changes

--- src/Pages/AboutPage/AboutPage.jsx
+++ src/Pages/AboutPage/AboutPage.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import Iphone from '../../Components/Iphone/Iphone';
-import IphoneX from '../../Components/Iphone/IphoneX';
-import Macbook from '../../Components/Macbook/Macbook';
-import OurTeam from '../../Components/OurTeam/OurTeam';
-import Title from '../../Components/Title/Title';
-import './AboutPage.styles.css';
-import Footer from '../../Components/Footer/Footer';
+import React, { useState, useEffect } from "react";
+import Iphone from "../../Components/Iphone/Iphone";
+import IphoneX from "../../Components/Iphone/IphoneX";
+import Macbook from "../../Components/Macbook/Macbook";
+import OurTeam from "../../Components/OurTeam/OurTeam";
+import Title from "../../Components/Title/Title";
+import "./AboutPage.styles.css";
+import Footer from "../../Components/Footer/Footer";
 
 const AboutPage = () => {
   const [isMobile, setMobile] = useState(window.innerWidth < 960);
@@ -15,16 +15,17 @@ const AboutPage = () => {
   };
 
   useEffect(() => {
-    window.addEventListener('resize', updateMedia);
-    return () => window.removeEventListener('resize', updateMedia);
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
   });
 
   return (
     <div className="aboutpage">
       <section className="aboutpage__device">
-
-      <Title name="about" />
-      <div className="aboutpage__deviceContainer">{isMobile ? <IphoneX /> : <Macbook />}</div>
+        <Title name="about" />
+        <div className="aboutpage__deviceContainer">
+          {/* {isMobile ? <IphoneX /> : <Macbook />} */}
+        </div>
       </section>
       <OurTeam />
       <Footer />
